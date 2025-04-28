b769706612a13e69d4b487df21a0a68828d5f915
Kean Ombion <keanombion@gmail.com>
2025-04-28 10:37:20 +0800

    Fixed errors on theme provider

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,7 +1,12 @@
-"use client";
+'use client';
 
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { ReactNode } from 'react';
+import { type ThemeProviderProps as NextThemeProviderProps } from 'next-themes';
+
+interface ThemeProviderProps extends Omit<NextThemeProviderProps, 'children'> {
+  children: ReactNode;
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
