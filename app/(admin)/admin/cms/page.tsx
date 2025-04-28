451f9bd4842ae4482cdc36fa1e2a5aed1682f06b
Kean Ombion <keanombion@gmail.com>
2025-04-28 10:30:09 +0800

    CMS page

--- app/(admin)/admin/cms/page.tsx
+++ app/(admin)/admin/cms/page.tsx
@@ -1,7 +1,14 @@
-export const CmsPage = () => {
-    return (
-        <div>
-            <p>Coming soon</p>
-        </div>
-    )
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'CMS Page',
+  description: 'Manage your CMS content',
+};
+
+export default function Page() {
+  return (
+    <div>
+      <h1>CMS Page</h1>
+    </div>
+  );
 }
